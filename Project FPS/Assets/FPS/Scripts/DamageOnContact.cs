102ba465eca7c691d026c1f68833781e6a3ee508
Pita Bread <pdoriadev@gmail.com>
2020-03-30 02:15:38 -0700

    Lava functionality Started jetpack intro area

---
+++ Project FPS/Assets/FPS/Scripts/DamageOnContact.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnContact : MonoBehaviour
+{
+    public bool shouldDamage = true;
+    public float damage = 1f;
+    public float timeBetweenDamages = 0.4f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Actor act = other.GetComponent<Actor>();
+        if (act)
+        {
+            Damageable dam = other.GetComponent<Damageable>();
+            if (dam && act.affiliation == 1)
+            {
+                StartCoroutine(DamageTilExitCo(dam));
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Actor act = other.GetComponent<Actor>();
+        if (act)
+        {
+            Damageable dam = other.GetComponent<Damageable>();
+            if (dam && act.affiliation == 1)
+            {
+                StopAllCoroutines();
+            }
+        }
+    }
+
+    IEnumerator DamageTilExitCo(Damageable dam)
+    {
+        float t = 0;
+        while (shouldDamage)
+        {
+            t -= Time.deltaTime;
+
+            if (t <= 0)
+            {
+                dam.InflictDamage(damage, false, gameObject);
+                t = timeBetweenDamages;
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+}
+
+
